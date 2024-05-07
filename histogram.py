1b1b82e9d047dd00d594fe02797686b0524c4371
rubeenasulthana <rubeena.sulthana@jukshio.com>
2024-05-07 10:33:52 +0530

    first commit

---
+++ histogram.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+ 
+img = cv.imread('/home/rubeena-sulthana/Desktop/experiment.s/ahad.jpeg')
+cv.imwrite("/home/rubeena-sulthana/Desktop/experiment.s/anki.png",img)
+assert img is not None, "file could not be read, check with os.path.exists()"
+color = ('b','g','r')
+for i,col in enumerate(color):
+ histr = cv.calcHist([img],[i],None,[256],[0,256])
+ plt.plot(histr,color = col)
+ plt.xlim([0,256])
+plt.show()
